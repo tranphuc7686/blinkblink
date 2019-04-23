28ff5312e052b3e00e776dd6ae11bf1f18afe329
Trần Phúc <phuc.tc@3si.vn>
2019-04-23 17:55:35 +0700

    Update code

---
+++ Blinkblink/Services/HomeHelper.cs
@@ -0,0 +1,42 @@
+﻿using Blinkblink.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blinkblink.Services
+{
+    public class HomeHelper
+    {
+        private DBBlinkContext _dBBlinkContext;
+        public HomeHelper(DBBlinkContext dBBlinkContext)
+        {
+            _dBBlinkContext = dBBlinkContext;
+        }
+        public void AddPathFilesProcess(string caption,string filePath, Idol idol,User user , int typeData)
+        {
+            Image image = new Image { Id = RandomId(16, true), Name = caption, DateTime = DateTime.Now, IdolId = idol.Id, UserId = user.Id, Url = filePath, IsVideo = typeData, IdImageRoot = "" };
+            _dBBlinkContext.Images.Add(image);
+            _dBBlinkContext.SaveChanges();
+
+        }
+        public string RandomId(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+    }
+   
+   
+}
---
+++ Blinkblink/ViewModels/UploadedImageData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blinkblink.ViewModels
+{
+    public class UploadedImageData
+    {
+        [Required]
+        public string Caption { get; set; }
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
